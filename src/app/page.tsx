3ac573928bc21151bd08469189204859839f03cd
Renan Gomes <renango2015@gmail.com>
2024-05-02 13:37:42 -0300

    fix: introduce 'useMounted' hook to mitigate hydration issues with random question rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import Image from 'next/image';
-import styles from './page.module.css';
 import { useCareerProfiler } from '@/hooks/useCareerProfiler';
+import { useMounted } from '@/hooks/useMounted';
 import { Fragment } from 'react';
+
+import styles from './page.module.css';
 
 export default function Home() {
   const {
@@ -11,37 +12,41 @@ export default function Home() {
     currentQuestions,
     isQuestionnaireComplete,
     handleGoToNextSlice,
-    handleGoToNextPrevious,
+    handleGoToPreviousSlice,
     handleCalculateResults,
     handleSetCurrentAnswers,
   } = useCareerProfiler();
 
+  const { isMounted } = useMounted();
+
   return (
     <main className={styles.main}>
-      {currentQuestions.map(({ id, question, answers }) => {
-        return (
-          <Fragment key={question}>
-            <p>{question}</p>
-            <div style={{ display: 'flex', gap: '2rem' }}>
-              {answers.map(({ opinion, careerScores }) => {
-                const isCurrentButtonSelected = currentAnswers[id] && currentAnswers[id].opinion === opinion;
+      {isMounted &&
+        currentQuestions.map(({ id, question, answers }) => {
+          return (
+            <Fragment key={question}>
+              <p>{question}</p>
+              <div style={{ display: 'flex', gap: '2rem' }}>
+                {answers.map(({ opinion, careerScores }) => {
+                  const isCurrentButtonSelected =
+                    currentAnswers[id] && currentAnswers[id].opinion === opinion;
 
-                return (
-                  <button
-                    key={opinion}
-                    style={{ color: isCurrentButtonSelected ? 'red' : 'white' }}
-                    onClick={() => handleSetCurrentAnswers(id, opinion, careerScores)}
-                  >
-                    {opinion}
-                  </button>
-                );
-              })}
-            </div>
-          </Fragment>
-        );
-      })}
+                  return (
+                    <button
+                      key={opinion}
+                      style={{ color: isCurrentButtonSelected ? 'red' : 'white' }}
+                      onClick={() => handleSetCurrentAnswers(id, opinion, careerScores)}
+                    >
+                      {opinion}
+                    </button>
+                  );
+                })}
+              </div>
+            </Fragment>
+          );
+        })}
       {isQuestionnaireComplete && <button onClick={handleCalculateResults}>Calcular</button>}
-      <button onClick={handleGoToNextPrevious}>Voltar</button>
+      <button onClick={handleGoToPreviousSlice}>Voltar</button>
       <button onClick={handleGoToNextSlice}>Proximo</button>
     </main>
   );
