e986e9cc57052b80f002ac7dd98d3bc2b68ff219
Renan Gomes <renango2015@gmail.com>
2024-05-30 04:29:06 -0300

    style: add 'Footer' styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,27 @@
+import logoVertical from '@/assets/logos/pngs/logo-horizontal.png';
+import logoUnifoa from '@/assets/logos/pngs/logo-unifoa.png';
+import styles from './Footer.module.scss';
+
 export const Footer = () => {
   return (
-    <footer>
-      <span>© {new Date().getFullYear()} - Todos os direitos reservados</span>
-      <div>
-        <span>MEDVR - 79ª Turma de Medicina da UniFOA ⚕️</span>
-        <span>UNIFOA - Centro Universitário de Volta Redonda</span>
+    <footer className={styles.footer}>
+      <div className={styles.copyrightWrapper}>
+        <ul className={styles.logoWrapper}>
+          <li className={styles.logoItem}>
+            <a className={styles.logoLink} href='https://www.unifoa.edu.br/'>
+              <img className={styles.logoUnifoa} src={logoUnifoa} alt='logo-unifoa' />
+            </a>
+          </li>
+          <li className={styles.logoItem}>
+            <a className={styles.logoLink} href='https://www.instagram.com/pontesdofuturo.unifoa'>
+              <img className={styles.logoHorizontal} src={logoVertical} alt='logo-horizontal' />
+            </a>
+          </li>
+        </ul>
+        <span className={styles.copyrightText}>
+          Copyright © {new Date().getFullYear()} - UniFOA | Todos os direitos reservados à Fundação Oswaldo
+          Aranha
+        </span>
       </div>
     </footer>
   );
