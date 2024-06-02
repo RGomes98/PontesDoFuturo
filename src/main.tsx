7b336e77db0041b14251bc30c1e9870a6a90bc75
Renan Gomes <renango2015@gmail.com>
2024-06-02 16:22:03 -0300

    refactor: update 'src' folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
+import { createRouter, Navigate, RouterProvider } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen.ts';
+
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import React from 'react';
+
+import '@/stylesheets/globals.scss';
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => Navigate({ to: '/', replace: true }),
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
