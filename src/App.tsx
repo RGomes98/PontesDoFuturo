89ed1cb0bad28c1fbdb05f3be00ab38e69d43c84
Renan Gomes <renango2015@gmail.com>
2024-06-02 16:02:33 -0300

    refactor: update '@tanstack/react-router' declaration order

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import { routeTree } from './routeTree.gen';
 
 import '@/stylesheets/globals.scss';
 
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => Navigate({ to: '/', replace: true }),
+});
+
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router;
   }
 }
-
-const router = createRouter({
-  routeTree,
-  defaultNotFoundComponent: () => Navigate({ to: '/', replace: true }),
-});
 
 function App() {
   return <RouterProvider router={router} />;
