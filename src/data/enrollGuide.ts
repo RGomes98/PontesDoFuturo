89a8bf4728a8115a41f7d5c7b1c4dfc7c905e8bf
Renan Gomes <renango2015@gmail.com>
2024-06-10 16:53:44 -0300

    chore: update enrollGuide section content

--- src/data/enrollGuide.ts
+++ src/data/enrollGuide.ts
@@ -2,44 +2,44 @@ export const enrollGuide = [
   {
     title: 'FIES',
     content:
-      'O FIES é um programa de financiamento estudantil que ajuda alunos a pagarem seus estudos em instituições privadas. Para se candidatar, é necessário atender a critérios, como renda familiar e ter feito o ENEM. O processo de inscrição envolve preencher formulários e enviar documentação dentro dos prazos estabelecidos. O financiamento é disponível para diversos cursos e instituições de ensino credenciadas. Os juros são baixos e o pagamento pode ser feito após a formatura, com condições flexíveis. Os estudantes são responsáveis por manter um bom desempenho acadêmico e cumprir as obrigações financeiras. O FIES pode proporcionar oportunidades de educação, mas também exige compromisso financeiro no futuro.',
+      'O FIES é um programa de financiamento estudantil que facilita o pagamento de estudos em instituições privadas. Para se candidatar, é necessário cumprir critérios como renda familiar e participação no ENEM. A inscrição exige preencher formulários e enviar documentos dentro dos prazos. O financiamento cobre diversos cursos e instituições credenciadas, com juros baixos e pagamento após a formatura. Manter bom desempenho acadêmico e cumprir obrigações financeiras é crucial. O FIES oferece oportunidades educacionais, mas requer comprometimento financeiro futuro.',
   },
   {
     title: 'ENEM',
     help: 'https://www.gov.br/inep/pt-br/areas-de-atuacao/avaliacao-e-exames-educacionais/enem',
     content:
-      'O ENEM é uma avaliação do desempenho escolar dos estudantes ao término da educação básica, sendo a principal forma de ingresso para o ensino superior no Brasil, além de também poder ser usada em algumas universidades no exterior. As principais formas de ingresso para as universidades com a nota do ENEM são: SISU, PROUNI e FIES. Essa prova é realizada em duas etapas, sendo composta por uma redação dissertativa-argumentativa, linguagens, ciências humanas, matemática e ciências da natureza.',
+      'O ENEM é uma avaliação essencial para quem deseja ingressar no ensino superior no Brasil. É uma prova composta por questões de linguagens, ciências humanas, matemática e ciências da natureza, além de uma redação dissertativa-argumentativa. As notas obtidas podem ser usadas no SISU, PROUNI e FIES, e até em algumas universidades internacionais. Realizado em duas etapas, o ENEM é a principal porta de entrada para diversas universidades brasileiras, tornando-se uma ferramenta crucial na trajetória acadêmica de muitos estudantes.',
   },
   {
     title: 'PROUNI',
     help: 'https://acessounico.mec.gov.br/prouni',
     content:
-      'O Prouni (Programa Universidade para Todos) pode garantir acesso a uma faculdade particular a partir de bolsas de estudos integrais (cobrem o custo completo) ou parciais (cobrem uma parcela do custo). Para conseguir ingressar no programa, é necessário realizar o ENEM e atingir uma nota igual ou superior à nota de corte (que varia a cada ano). Além disso, é necessário ter cursado o Ensino Médio completo em escola pública ou em escola privada como bolsista integral. Os algoritmos do PROUNI também dão preferência a conceder bolsa para pessoas portadoras de alguma deficiência e para funcionários públicos.',
+      'O Prouni (Programa Universidade para Todos) oferece bolsas integrais ou parciais para faculdades particulares. Para participar, é necessário ter feito o ENEM e alcançar uma nota de corte, que varia anualmente. Além disso, o candidato deve ter cursado o Ensino Médio em escola pública ou como bolsista integral em escola privada. O PROUNI prioriza pessoas com deficiência e funcionários públicos, promovendo inclusão e oportunidades educacionais para quem não pode arcar com os custos do ensino superior. É uma alternativa valiosa para acesso à educação superior de qualidade.',
   },
   {
     title: 'Olimpíadas Estudantis',
     content:
-      'Ingresso pela trilha olímpica é um modelo de admissão adotado por algumas instituições públicas de ensino superior, reservando vagas para medalhistas de olimpíadas científicas em cursos específicos. Esse sistema foi implementado em 2019 e tem ganhado popularidade, com mais universidades adotando-o. As vagas são destinadas aos medalhistas de ouro, prata e bronze nessas competições, visando evitar que jovens talentos sejam subutilizados no processo tradicional de vestibular e garantir que contribuam para suas áreas de conhecimento.',
+      'A trilha olímpica é um modelo de ingresso adotado por algumas universidades públicas para medalhistas de olimpíadas científicas. Implementado em 2019, reserva vagas para alunos que conquistaram medalhas de ouro, prata ou bronze, visando aproveitar talentos que poderiam ser subutilizados no vestibular tradicional. A crescente adoção deste sistema por diversas instituições visa incentivar jovens talentos a contribuir nas áreas de conhecimento em que se destacam, promovendo uma alternativa meritocrática e focada em suas habilidades excepcionais.',
   },
   {
     title: 'Cursos Técnicos',
     content:
-      'Os cursos técnicos encaminham os estudantes direto para o mercado de trabalho. Se encaixam entre o ensino médio e o ensino superior, ajudando o estudante a estar apto para ter uma profissão assim que se forma no ensino regular, antes mesmo de entrar em uma faculdade.',
+      'Os cursos técnicos preparam estudantes diretamente para o mercado de trabalho, posicionando-se entre o ensino médio e superior. Eles equipam os alunos com habilidades práticas para que possam iniciar uma carreira logo após completar o ensino regular, sem a necessidade de um diploma universitário imediato. Esses cursos são ideais para quem deseja entrar rapidamente no mercado de trabalho com uma formação específica e prática, possibilitando o exercício de uma profissão assim que se forma no ensino médio, antes de buscar educação superior.',
   },
   {
     title: 'Concursos Públicos',
     help: 'https://www.gov.br/gestao/pt-br/concursonacional',
     content:
-      'O concurso público é uma prova realizada para exercer um cargo público em áreas como segurança (polícia militar), educação, saúde, administração pública e muitos outros. As provas são teóricas e podem incluir testes físicos ou psicológicos. As provas teóricas ainda podem ser divididas em conhecimentos gerais (abordando matérias como português e matemática) e provas com conhecimentos específicos para o cargo escolhido. As provas têm um número limitado de vagas a serem preenchidas e os candidatos que conseguirem alcançar as maiores notas serão chamados para exercer o cargo. Muitos cargos públicos não exigem curso superior (faculdade) ou especialização e você pode realizar a prova assim que concluir o Ensino Médio! Acesse aqui uma lista dos concursos e áreas que você pode ingressar assim que completar o ensino médio, sem necessidade de realizar o Ensino Superior O Concurso Público Nacional Unificado é o “ENEM” dos concursos públicos. Ele unifica todas as oportunidades de carreira em uma única prova.',
+      'O concurso público é uma prova para ingresso em cargos públicos em áreas como segurança, educação, saúde e administração. Além de testes teóricos, pode incluir avaliações físicas ou psicológicas. As provas teóricas são divididas em conhecimentos gerais e específicos. Há um número limitado de vagas e os candidatos com melhores notas são convocados. Muitos cargos não exigem curso superior, permitindo que pessoas com Ensino Médio participem. O Concurso Público Nacional Unificado, semelhante ao ENEM, unifica oportunidades de carreira em uma prova única, facilitando o ingresso no setor público.',
   },
   {
     title: 'Vestibulares e Cursinhos',
     content:
-      'O vestibular é um método empregado por instituições de ensino superior, com o propósito de selecionar candidatos para ingresso em diferentes cursos. Esses exames têm como objetivo avaliar o conhecimento e as habilidades dos candidatos, determinando quem será admitido em cada curso. Já o curso pré-vestibular, conhecido como cursinho, é um programa educacional projetado para preparar estudantes que pretendem realizar o exame vestibular ou o ENEM - (Exame Nacional do Ensino Médio). Ele oferece instrução intensiva em disciplinas-chave, revisão de conteúdo e prática de questões, visando aumentar as chances de sucesso dos alunos nos exames de admissão às instituições de ensino superior.',
+      'O vestibular é um exame usado por instituições de ensino superior para selecionar estudantes para seus cursos. Ele avalia conhecimentos e habilidades dos candidatos, determinando quem será admitido. O cursinho pré-vestibular é um programa intensivo que prepara estudantes para o vestibular ou ENEM, oferecendo revisões de conteúdo e prática de questões. Esses programas aumentam as chances de sucesso dos alunos nos exames de admissão, proporcionando uma preparação sólida e direcionada para enfrentar as exigências das universidades.',
   },
   {
     title: 'Jovem Aprendiz',
     content:
-      'O Jovem Aprendiz é uma modalidade de emprego que busca a capacitação, orientação e inserção de pessoas de 14 a 24 anos no mercado. Esses jovens, que obrigatoriamente precisam estar frequentando o ensino fundamental ou médio, executam tarefas e atividades em empresas públicas ou privadas. Para as empresas, significa a oportunidade de contar com uma mão de obra desenvolvida conforme os seus critérios e cultura, por meio de uma estratégia de responsabilidade social que considera o aprimoramento e qualificação dessas pessoas. E para o jovem aprendiz resulta em uma chance de fortalecer esse desenvolvimento na área, com uma rotina condizente com o seu perfil e com a oportunidade de conquista de sua primeira chance no mercado de trabalho. No fim desse período de experiência, é concedida uma certificação, responsável por comprovar e atestar a qualificação desenvolvida, que trará um valor maior e diferencial ao currículo desse colaborador.',
+      'O Jovem Aprendiz é um programa que capacita e insere jovens de 14 a 24 anos no mercado de trabalho. Esses jovens, que devem estar estudando, realizam atividades em empresas públicas ou privadas. Para as empresas, isso representa uma forma de desenvolver mão de obra conforme suas necessidades e promover responsabilidade social. Para o aprendiz, é uma oportunidade de ganhar experiência prática e obter uma certificação ao final do período, valorizando seu currículo e aumentando suas chances de sucesso profissional em sua primeira experiência no mercado de trabalho.',
   },
 ];
