40542beecef1dfb569604bc079ba56a4c7f2baff
Renan Gomes <renango2015@gmail.com>
2024-06-19 00:01:22 -0300

    fix: address 'TS2589: Type instantiation is excessively deep and possibly infinite.'

--- src/hooks/useCarousel.ts
+++ src/hooks/useCarousel.ts
@@ -8,21 +8,21 @@ const options: IntersectionObserverInit = {
 };
 
 export const useCarousel = () => {
-  const [isControlOnCooldown, setIsControlOnCooldown] = useState(false);
+  const [isControlOnCooldown, setIsControlOnCooldown] = useState<boolean>(false);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState<number>(0);
   const [carouselSlides, setCarouselSlides] = useState<Element[]>([]);
-  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
   const carouselRef = useRef<HTMLUListElement>(null);
   const { pathname } = useLocation();
 
-  function carouselControls(direction: 'previous' | 'next') {
+  function carouselControls(direction: 'previous' | 'next'): void {
     const CAROUSEL_CONTROL_COOLDOWN = 500;
     if (isControlOnCooldown) return;
     setIsControlOnCooldown(true);
 
     setCurrentSlideIndex((currentSlide) => {
-      const previous = carouselSlides[currentSlide].previousElementSibling;
-      const next = carouselSlides[currentSlide].nextElementSibling;
+      const previous = carouselSlides[currentSlide]?.previousElementSibling;
+      const next = carouselSlides[currentSlide]?.nextElementSibling;
 
       const previousSlide = previous ? carouselSlides.indexOf(previous) : currentSlide;
       const nextSlide = next ? carouselSlides.indexOf(next) : currentSlide;
@@ -35,12 +35,12 @@ export const useCarousel = () => {
     setTimeout(() => setIsControlOnCooldown(false), CAROUSEL_CONTROL_COOLDOWN);
   }
 
-  function scrollToSlide(index: number) {
-    carouselSlides[index].scrollIntoView({ inline: 'center', block: 'nearest', behavior: 'smooth' });
+  function scrollToSlide(index: number): void {
+    carouselSlides[index]?.scrollIntoView({ inline: 'center', block: 'nearest', behavior: 'smooth' });
   }
 
   useEffect(() => {
-    (function resetCarouselSlides() {
+    (function resetCarouselSlides(): void {
       const carousel = carouselRef.current;
       if (!carousel) return;
 
@@ -53,9 +53,11 @@ export const useCarousel = () => {
   useEffect(() => {
     const intersectionObserver = new IntersectionObserver(callback, options);
 
-    function callback(entries: IntersectionObserverEntry[]) {
+    function callback(entries: IntersectionObserverEntry[]): void {
       entries.forEach((slide) => {
-        slide.isIntersecting && setCurrentSlideIndex(carouselSlides.indexOf(slide.target));
+        if (slide.isIntersecting) {
+          setCurrentSlideIndex(carouselSlides.indexOf(slide.target));
+        }
       });
     }
 
